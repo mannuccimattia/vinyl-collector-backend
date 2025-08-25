2cb8c3bf29c0d5e0408ffb1d6e9d629ef8f3bea9
mannuccimattia <mannuccimattia@gmail.com>
2025-08-25 16:04:40 +0200

    [UPD] Customized default layout + auth views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,26 +1,47 @@
 @extends('layouts.app')
 
+@section('title')
+    {{ Auth::user()->name }} | Dashboard
+@endsection
+
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+    <div class="container">
+        <h2 class="fs-4 text-secondary my-4">
+            {{ __('Dashboard') }}
+        </h2>
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="card dark mt-4">
+                    <div class="card-header dark">
+                        {{ Auth::user()->name }}
                     </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in!') }}
+                        <div class="d-flex mt-3 gap-3">
+                            <p>
+                                <a href="{{ url('/vinyls') }}">
+                                    {{ __('Vinyls') }}</a>
+                            </p>
+                            <p>
+                                <a href="{{ url('/labels') }}">
+                                    {{ __('Labels') }}</a>
+                            </p>
+                            <p>
+                                <a href="{{ url('/genres') }}">
+                                    {{ __('Genres') }}</a>
+                            </p>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
---
+++ resources/views/vinyls.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title')
+    Collection | Vinyl Collector
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h1>hello collection</h1>
+        </div>
+    </div>
+@endsection
