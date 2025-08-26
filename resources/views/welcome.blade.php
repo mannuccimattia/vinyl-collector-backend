42658d9ebfad0e85a0bc998a22046d08635a4093
mannuccimattia <mannuccimattia@gmail.com>
2025-08-26 15:25:20 +0200

    [FIX] Restored accidentally deleted css rule

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
             <div class="col-5">
                 <a href="{{ route('vinyls.index') }}" class="card dark rounded-pill border-black text-decoration-none">
                     <div class="card-content d-flex align-items-center justify-content-between">
-                        <img class="img-fluid card-image"
+                        <img class="img-fluid welcome-card-image"
                             src="{{ Vite::asset('resources/img/logo/vinylcollector-black-disc.png') }}" alt="">
                         <h3 class="pe-3">Vinyls</h3>
                     </div>
@@ -21,7 +21,7 @@
             <div class="col-5">
                 <a href="" class="card dark rounded-pill border-white text-decoration-none">
                     <div class="card-content d-flex align-items-center justify-content-between">
-                        <img class="img-fluid card-image"
+                        <img class="img-fluid welcome-card-image"
                             src="{{ Vite::asset('resources/img/logo/vinylcollector-white-disc.png') }}" alt="">
                         <h3 class="pe-3">Labels</h3>
                     </div>
@@ -31,7 +31,7 @@
             <div class="col-5">
                 <a href="" class="card dark rounded-pill border-black text-decoration-none">
                     <div class="card-content d-flex align-items-center justify-content-between">
-                        <img class="img-fluid card-image"
+                        <img class="img-fluid welcome-card-image"
                             src="{{ Vite::asset('resources/img/logo/vinylcollector-black-disc.png') }}" alt="">
                         <h3 class="pe-3">Genres</h3>
                     </div>
