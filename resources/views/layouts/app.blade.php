14d210f013fba150f257e1329f6fb9c54ea87d08
mannuccimattia <mannuccimattia@gmail.com>
2025-09-01 15:51:00 +0200

    [FIX] Fixed FOUC issue when navigating via UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    {{-- Added to fix FOUC on UI navigation --}}
+    <link rel="preload" href="{{ asset('scss/app.scss') }}" as="style">
+    <link rel="stylesheet" href="{{ asset('scss/app.scss') }}">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
